52f5b405107852d7f22020e24589ad9b7254b112
Eduard Dzhumagaliev <terriblegoodday@icloud.com>
2022-01-30 17:24:09 +1000

    Add file support

--- client.py
+++ client.py
@@ -102,12 +102,74 @@ def new_todo():
 	c.setopt(pycurl.POSTFIELDS, todo_json)
 	c.perform()
 	c.close()
+
+def upload_file():
+	input_filename = input(bcolors.OKGREEN + bcolors.BOLD + "Input your file name:" + bcolors.ENDC + bcolors.ENDC + " ")
+
+	file = str(open(input_filename, "r").buffer)
+
+	c = pycurl.Curl()
+	c.setopt(pycurl.CUSTOMREQUEST, "POST")
+	c.setopt(pycurl.URL, "127.0.0.1:8080/files/" + input_filename)
+	c.setopt(pycurl.FOLLOWLOCATION, 1)
+	c.setopt(pycurl.POSTFIELDS, file)
+	
+	c.perform()
+	c.close()
+
+def get_file():
+	input_filename = input(bcolors.OKGREEN + bcolors.BOLD + "Input your file name:" + bcolors.ENDC + bcolors.ENDC + " ")
+
+	with open(input_filename, "wb") as file:
+		c = pycurl.Curl()
+		c.setopt(pycurl.CUSTOMREQUEST, "GET")
+		c.setopt(pycurl.URL, "127.0.0.1:8080/files/" + input_filename)
+		c.setopt(pycurl.FOLLOWLOCATION, 1)
+		c.setopt(pycurl.WRITEDATA, file)
+		
+		c.perform()
+		c.close()
+
+def get_files():
+	c = pycurl.Curl()
+	c.setopt(pycurl.CUSTOMREQUEST, "GET")
+	c.setopt(pycurl.URL, "127.0.0.1:8080/files")
+	c.setopt(pycurl.FOLLOWLOCATION, 1)
+
+	buffer = io.BytesIO()
+	
+	c.setopt(c.WRITEFUNCTION, buffer.write)
+	c.perform()
+	c.close()
+	
+	response = buffer.getvalue()
+	
+	json_response = json.loads(response)
+	
+	for file in json_response:
+		print(bcolors.OKBLUE + bcolors.BOLD + file.get("name") + " " + bcolors.ENDC + bcolors.ENDC + str(file["size"]))
+
+def remove_file():
+	input_filename = input(bcolors.OKGREEN + bcolors.BOLD + "Input your file name:" + bcolors.ENDC + bcolors.ENDC + " ")
+
+	c = pycurl.Curl()
+	c.setopt(pycurl.CUSTOMREQUEST, "DELETE")
+	c.setopt(pycurl.URL, "127.0.0.1:8080/files/" + input_filename)
+	c.setopt(pycurl.FOLLOWLOCATION, 1)
+	
+	c.perform()
+	c.close()
+
 	
 def main_menu():
 	print("Choose your endpoint:")
 	print(bcolors.OKBLUE + bcolors.BOLD + "[1] " + bcolors.ENDC + bcolors.ENDC + "Get todos")
 	print(bcolors.OKBLUE + bcolors.BOLD + "[2] " + bcolors.ENDC + bcolors.ENDC + "New todo")
-	print(bcolors.WARNING + bcolors.BOLD + "[3] " + bcolors.ENDC + bcolors.ENDC + "Exit")
+	print(bcolors.OKBLUE + bcolors.BOLD + "[3] " + bcolors.ENDC + bcolors.ENDC + "Upload file")
+	print(bcolors.OKBLUE + bcolors.BOLD + "[4] " + bcolors.ENDC + bcolors.ENDC + "Download file")
+	print(bcolors.OKBLUE + bcolors.BOLD + "[5] " + bcolors.ENDC + bcolors.ENDC + "Index files")
+	print(bcolors.OKBLUE + bcolors.BOLD + "[6] " + bcolors.ENDC + bcolors.ENDC + "Remove file")
+	print(bcolors.WARNING + bcolors.BOLD + "[7] " + bcolors.ENDC + bcolors.ENDC + "Exit")
 	
 	input_number = input("Input number: ")
 	
@@ -115,8 +177,16 @@ def main_menu():
 		get_todos()
 	if input_number == "2":
 		new_todo()
+	if input_number == "3":
+		upload_file()
+	if input_number == "4":
+		get_file()
+	if input_number == "5":
+		get_files()
+	if input_number == "6":
+		remove_file()
 	
-	return input_number == "3"
+	return input_number == "7"
 	
 is_authenticated = False
 user = None
